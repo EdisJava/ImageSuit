f3110c1ead52c23e99815b6c39bced21480c6d00
Edgar <edgarjqm122@gmail.com>
2026-01-26 11:16:29 +0000

    Traduccion

--- SuiteUI/translations/app_es.ts
+++ SuiteUI/translations/app_es.ts
@@ -4,12 +4,12 @@
 <context>
     <name>DownloadWidget</name>
     <message>
-        <location filename="downloadwidget.ui" line="14"/>
+        <location filename="../downloadwidget.ui" line="14"/>
         <source>Form</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="downloadwidget.ui" line="160"/>
+        <location filename="../downloadwidget.ui" line="63"/>
         <source>Available downloads</source>
         <translation>Disponibles para descargar</translation>
     </message>
@@ -17,27 +17,31 @@
 <context>
     <name>DownloadedWidget</name>
     <message>
-        <location filename="downloadedwidget.ui" line="14"/>
+        <location filename="../downloadedwidget.ui" line="14"/>
         <source>Form</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="downloadedwidget.ui" line="74"/>
+        <location filename="../downloadedwidget.ui" line="107"/>
         <source>Downloaded       </source>
         <translation>Descargados       </translation>
     </message>
     <message>
-        <location filename="downloadedwidget.ui" line="114"/>
-        <source>lorem ipsum.png      </source>
-        <translation></translation>
+        <location filename="../downloadedwidget.ui" line="286"/>
+        <source>Change View</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="downloadedwidget.ui" line="142"/>
+        <location filename="../downloadedwidget.ui" line="305"/>
+        <source>Toggle Favourite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Show Favorites</source>
-        <translation>Filtrar Favoritos</translation>
+        <translation type="vanished">Filtrar Favoritos</translation>
     </message>
     <message>
-        <location filename="downloadedwidget.ui" line="346"/>
+        <location filename="../downloadedwidget.ui" line="267"/>
         <source>Introduce the name who are you searching for</source>
         <translation>Introduce el nombre para buscar</translation>
     </message>
@@ -45,7 +49,7 @@
 <context>
     <name>ImageCardDelegate</name>
     <message>
-        <location filename="imagecarddelegate.ui" line="14"/>
+        <location filename="../imagecarddelegate.ui" line="14"/>
         <source>Form</source>
         <translation></translation>
     </message>
@@ -53,17 +57,17 @@
 <context>
     <name>ImageViewer</name>
     <message>
-        <location filename="imageviewer.ui" line="14"/>
+        <location filename="../imageviewer.ui" line="14"/>
         <source>Form</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="imageviewer.ui" line="70"/>
+        <location filename="../imageviewer.ui" line="72"/>
         <source>lorem impsum.png</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="imageviewer.ui" line="100"/>
+        <location filename="../imageviewer.ui" line="103"/>
         <source>lorem ipsum is a picture</source>
         <translation></translation>
     </message>
@@ -71,7 +75,7 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.ui" line="14"/>
         <source>MainWindow</source>
         <translation>Suite</translation>
     </message>
